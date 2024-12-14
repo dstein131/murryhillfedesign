b5471d7a0aef989162b1c1ae82510829099598dd
David Stein <dstein131@gmail.com>
2024-12-14 02:33:28 -0500

    1

--- my-react-app/src/pages/Music.jsx
+++ my-react-app/src/pages/Music.jsx
@@ -5,17 +5,17 @@ const Music = () => {
     {
       title: 'Sleep Token - Hypnosis An offering from II',
       description: 'One of my favorite drummers. This song helps me focus when I work.',
-      videoUrl: 'https://www.youtube.com/embed/nNZlxQmpV8g', // Embed URL
+      videoUrl: 'https://www.youtube.com/embed/nNZlxQmpV8g',
     },
     {
       title: 'The Yussef Dayes Experience - Full Performance (Live on KEXP)',
       description: 'Another brilliant drummer, great modern jazz fusion.',
-      videoUrl: 'https://www.youtube.com/embed/S1BuRprwXNc', // Embed URL
+      videoUrl: 'https://www.youtube.com/embed/S1BuRprwXNc',
     },
     {
       title: 'Mac DeMarco - Chamber Of Reflection',
       description: 'Great all-round vibes.',
-      videoUrl: 'https://www.youtube.com/embed/pQsF3pzOc54', // Embed URL
+      videoUrl: 'https://www.youtube.com/embed/pQsF3pzOc54',
     },
   ];
 
@@ -28,7 +28,6 @@ const Music = () => {
       <main className="music-page__main">
         {musicList.map((music, index) => (
           <div key={index} className="music-card">
-            <h2 className="music-card__title">{music.title}</h2>
             <iframe
               className="music-card__video"
               src={music.videoUrl}
@@ -37,6 +36,7 @@ const Music = () => {
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
             ></iframe>
+            <h2 className="music-card__title">{music.title}</h2>
             <p className="music-card__description">{music.description}</p>
           </div>
         ))}
