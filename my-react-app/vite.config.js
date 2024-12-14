8f6e4d28653a452c579912c68183e5bac8c5b981
David Stein <dstein131@gmail.com>
2024-12-13 22:24:42 -0500

    1

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,12 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000, // Sets the dev server to run on localhost:3000
-    // Optional: Specify the host if needed
-    // host: 'localhost',
-  },
+  base: '/', // Set this to '/' for a root-level deployment
 });
