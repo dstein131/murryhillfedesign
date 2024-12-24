9876a3f0f46e38aa1131ba76a2c4562a48096601
David Stein <dstein131@gmail.com>
2024-12-23 22:50:08 -0500

    2

--- my-react-app/src/pages/Templates.jsx
+++ my-react-app/src/pages/Templates.jsx
@@ -22,7 +22,7 @@ const Templates = () => {
       id: 15,
       title: 'MHWD Basic Services Template',
       description: 'A basic services template for a variety of businesses. This template is a modern responsive design that is veristile and can be tailored to any business.',
-      imageUrl: '/images/templates/MHWD_Services.png',
+      imageUrl: '/images/templates/plumberweb.png',
       link: 'https://harmonious-madeleine-1f69a2.netlify.app/',
     },
     {
